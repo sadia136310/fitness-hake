e71762a22defb33ab024833e677c2b16d949bb54
sadia <saimislam64@gnail.com>
2022-09-29 06:40:52 +0600

    update tostify

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,16 +3,26 @@ import React, { useState } from 'react';
 import './Cart.css'
 import img from '../../images/280115748_1643689546029636_4366612206964565876_n.jpg'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 const Cart = ({cart}) => {
-    // console.log(cart);
+    console.log(cart);
 let time=0;
 for(const data of cart){
    time= time + parseInt(data.time);
 }
 const [show,setShow]= useState(0)
-const handleShow=()=>setShow(10);
+const handleShow1=()=>setShow(0+10);
+const handleShow2=()=>setShow(0+20);
+const handleShow3=()=>setShow(0+30);
+const handleShow4=()=>setShow(0+40);
+const handleShow5=()=>setShow(0+50);
 
-
+const notify = () => {
+    toast("Wow!! Successfully Completed.");
+}
 
     return (
            <div>
@@ -37,31 +47,35 @@ const handleShow=()=>setShow(10);
                     <p>age</p>
                 </div>
             </button>
-            <br />
+         
             <p>Add a break</p>
             
             <button className='other-container'>
-               <button onClick={handleShow} className='btn-1'>10s</button>
-               <button className='btn-1'>20s</button>
-               <button className='btn-1'>30s</button>
-               <button className='btn-1'>40s</button>
-               <button className='btn-1'>50s</button>
+               <button onClick={handleShow1} className='btn-1'>10</button>
+
+               <button onClick={handleShow2} className='btn-1'>20</button>
+
+               <button onClick={handleShow3} className='btn-1'>30</button>
+
+               <button onClick={handleShow4} className='btn-1'>40</button>
+
+               <button onClick={handleShow5} className='btn-1'>50</button>
             </button>
-            <br />
             <p>Exercise Details</p>
             <button className='ex-time'>
                <h4> Exercise time </h4>
-                <p> {time}sec</p>
+                <p> {time} sec</p>
                 </button>
             <br />
-            <br />
+           
             <button className='br-time'>
             <h4> Break time  </h4>
-                <p>{show} sec</p>
+                <p> {show} sec</p>
             </button>
             <br />
-            <br />
-            <button className='activity'>Activity Completed</button>
+          
+            <button onClick={notify} className='activity'>Activity Completed</button>
+            <ToastContainer />
         </div>
     );
 };
